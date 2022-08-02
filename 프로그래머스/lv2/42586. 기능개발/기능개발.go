6c9926018a6027f13c9e3d1e4638e6238fe1b77b
GS.Hyeon <qlqlqkqlqn@gmail.com>
2022-08-02 16:55:28 +0900

    [level 2] Title: 기능개발, Time: 0.00 ms, Memory: 3.45 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.go
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.go
@@ -1,29 +1,23 @@
-func remainDates(progress, speed int) int {
-	remain := (100 - progress) / speed
-	if (100 - progress)%speed != 0 {
-		remain++
-	}
-	return remain
-}
-
 func solution(progresses []int, speeds []int) []int {
-	var result []int
+    var result []int
 
-	remains := make([]int, len(progresses))
-	for i := 0; i < len(progresses); i++ {
-		remains[i] = remainDates(progresses[i], speeds[i])
-	}
+    for len(progresses) > 0 {
+        if progresses[0] >= 100 {
+            i := 1
+            for ; i < len(progresses); i++ {
+                if progresses[i] < 100 {
+                    break
+                }
+            }
+            progresses = progresses[i:]
+            speeds = speeds[i:]
+            result = append(result, i)
+        } else {
+            for i := 0; i < len(progresses); i++ {
+                progresses[i] += speeds[i]
+            }
+        }
+    }
 
-	for len(remains) > 0 {
-		i := 1
-		for ; i < len(remains); i++ {
-			if remains[0] < remains[i] {
-				break
-			}
-		}
-		remains = remains[i:]
-		result = append(result, i)
-	}
-
-	return result
+    return result
 }
